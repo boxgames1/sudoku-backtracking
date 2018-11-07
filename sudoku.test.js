6f564ec8cd4c6dba3aeb8e5995b754d1b238d7d9
Oliver Alonso <cheapgames138@gmail.com>
2018-11-07 21:45:15 +0100

    Add column and row tests

--- sudoku.test.js
+++ sudoku.test.js
@@ -1,6 +1,6 @@
 import {solveSudoku} from './sudoku';
 
-const sudoku = [
+const unsolvedSudoku = [
     [0,0,8,4,0,3,5,0,6],
     [0,0,3,1,0,2,0,0,4],
     [0,4,5,7,0,0,0,9,0],
@@ -12,16 +12,29 @@ const sudoku = [
     [8,0,6,9,0,1,3,0,0]
 ];
 
-test('sudoku solution not null', () => {
-    const solvedSudoku = solveSudoku(sudoku);
-    expect(solvedSudoku).not.toBe(null);
+
+
+test('solved sudoku columns sum 45', () => {
+    const sudoku = unsolvedSudoku;
+    if(solveSudoku(sudoku, 0, 0)){
+        for(let i = 0; i<9; i++){
+            let sum = 0;
+            for(let j = 0; j<9; j++){
+                sum += sudoku[j][i];
+            }
+            expect(sum).toBe(45);
+        }
+    }
 });
 
+
 test('solved sudoku rows sum 45', () => {
-    const solvedSudoku = solveSudoku(sudoku);
-    solvedSudoku.forEach( row => {
-        let sum = 0;
-        row.forEach(cell => sum+=cell);
-        expect(sum).toBe(45);
-    })
+    const sudoku = unsolvedSudoku;
+    if(solveSudoku(sudoku, 0, 0)){
+        sudoku.forEach( row => {
+            let sum = 0;
+            row.forEach(cell => sum+=cell);
+            expect(sum).toBe(45);
+        })
+    }
 });
