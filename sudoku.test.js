cb2f550c649d48011a94c2688b0a5de3a6eb3039
Oliver Alonso <cheapgames138@gmail.com>
2018-11-07 22:13:53 +0100

    Add each solved sudoku row has all numbers from 1 to 9 test

--- sudoku.test.js
+++ sudoku.test.js
@@ -51,6 +51,22 @@ test("solved sudoku 3x3 boxes sum 45", () => {
   }
 });
 
+test("each solved sudoku row has all numbers from 1 to 9", () => {
+  const sudoku = unsolvedSudoku;
+  if (solveSudoku(sudoku, 0, 0)) {
+    sudoku.forEach(row => {
+      const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+      row.forEach(cell => {
+        const index = numbers.indexOf(cell);
+        if (index !== -1) {
+          numbers.splice(index, 1);
+        }
+      });
+      expect(numbers.length).toBe(0);
+    });
+  }
+});
+
 function getBoxSum(grid, row, col) {
   if (row > 6 || row < 0 || col < 0 || col > 6) return 0;
   let sum = 0;
